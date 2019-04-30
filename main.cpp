65fe9f1812be465b4b9eb62e4b2683a9b9f06973
Aloïs Micard <amicard@eurodns.com>
2019-04-30 08:20:10 +0200

    Configure OpenGL context

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ int main()
         return -1;
     }
 
+    // Configure OpenGL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Create the window and his OpenGL context
     pWindow = glfwCreateWindow(640, 480, "Hello world", nullptr, nullptr);
     if (!pWindow)
