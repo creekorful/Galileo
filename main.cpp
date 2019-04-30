d18648462296f3b3ab7d679a22ff55daba7a615d
Aloïs Micard <amicard@eurodns.com>
2019-04-30 08:13:59 +0200

    Create window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    GLFWwindow *pWindow;
+
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Unable to initialize GLFW");
+        return -1;
+    }
+
+    // Create the window and his OpenGL context
+    pWindow = glfwCreateWindow(640, 480, "Hello world", nullptr, nullptr);
+    if (!pWindow)
+    {
+        fprintf(stderr, "Unable to create window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make window context current
+    glfwMakeContextCurrent(pWindow);
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
