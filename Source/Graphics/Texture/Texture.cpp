e3555477ce2beb9cc173032835c605284c3891ef
Aloïs Micard <amicard@eurodns.com>
2019-05-03 14:26:05 +0200

    Add texture class

---
+++ Source/Graphics/Texture/Texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aloïs Micard on 2019-05-03.
+//
+
+#include "Texture.h"
---
+++ Source/Graphics/Texture/Texture.h
@@ -0,0 +1,14 @@
+#ifndef GALILEO_TEXTURE_H
+#define GALILEO_TEXTURE_H
+
+#include "../../Common.h"
+
+class Texture
+{
+public:
+private:
+    GLint _textureId;
+};
+
+
+#endif //GALILEO_TEXTURE_H
