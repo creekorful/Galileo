6b04cf6a5984e2131432bc2b107f2030c43afaca
Aloïs Micard <lunamicard@gmail.com>
2019-05-10 16:51:13 +0200

    Add operations on Vector2

--- Source/Math/Vector2.h
+++ Source/Math/Vector2.h
@@ -1,6 +1,7 @@
 #ifndef GALILEO_VECTOR2_H
 #define GALILEO_VECTOR2_H
 
+#include <cmath>
 
 template<class T>
 class Vector2
@@ -15,6 +16,34 @@ public:
     {
         this->x = x;
         this->y = y;
+    }
+
+    Vector2& Normalize()
+    {
+        double inverse = 1.0 / sqrt(x * x + y * y);
+        x *= inverse;
+        y *= inverse;
+        return *this;
+    }
+
+    Vector2 operator-(const Vector2& other)
+    {
+        return Vector2(x - other.x, y - other.y);
+    }
+
+    Vector2& operator-=(const Vector2& other)
+    {
+        x -= other.x;
+        y -= other.y;
+
+        return *this;
+    }
+
+    static Vector2 Normalize(const Vector2& vector)
+    {
+        double inverse = 1.0 / sqrt(vector.x * vector.x + vector.y * vector.y);
+
+        return Vector2(vector.x * inverse, vector.y * inverse);
     }
 };
 
